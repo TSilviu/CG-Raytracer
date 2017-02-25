3fc650e27b688dd4b1ca661f4d52e545a27d22a0
Mihail Calin Ionescu <ionescu.calin.m@gmail.com>
2017-02-25 14:57:34 +0000

    trying out stuff

--- Source/skeleton.cpp
+++ Source/skeleton.cpp
@@ -223,7 +223,7 @@ void Draw(const vector<Triangle>& triangles)
 	if( SDL_MUSTLOCK(screen) )
 		SDL_LockSurface(screen);
 
-	#pragma omp parallel for //firstprivate(color)
+	//#pragma omp parallel for //firstprivate(color)
 	for( int y=0; y<SCREEN_HEIGHT; ++y )
 	{
 		for( int x=0; x<SCREEN_WIDTH; ++x )
@@ -300,13 +300,13 @@ bool ClosestIntersection(const vec3 start, const vec3 dir, const vector<Triangle
 }
 
 vec3 DirectLight( const Intersection& i, const vector<Triangle>& triangles  ) {
-	#ifdef TEXTURES_CIMG
+	// #ifdef TEXTURES_CIMG
 		//vec2 bary_coords = barycentricCoordinates(triangles[i.triangleIndex], i.position);
-		vec3 file_normal = pixelFromTexture(vec2(i.u,i.v), nMap)*2.0f - 1.0f;
-		vec3 n = normalize(perturbedNormal(triangles[i.triangleIndex]) * file_normal);		//The triangle's normal
-	#else
+		// vec3 file_normal = pixelFromTexture(vec2(i.u,i.v), nMap)*2.0f - 1.0f;
+		// vec3 n = normalize(perturbedNormal(triangles[i.triangleIndex]) * file_normal);		//The triangle's normal
+	// #else
 		vec3 n = triangles[i.triangleIndex].normal;
-	#endif
+	// #endif
 	vec3 average = vec3(0.f,0.f,0.f);
 	Intersection objToLight;
 	for(int sample = 0; sample < SoftShadowsSamples; ++sample) {
@@ -388,8 +388,16 @@ void ApplyAntiAliasing(int x, int y, vec3& color, const vector<Triangle>& triang
 		if(ClosestIntersection(camera, cameraR*dir, triangles, inter)) {
 			vec3 directLight = DirectLight(inter, triangles);
 			#ifdef TEXTURES_CIMG 
-				//vec2 bary_coords = barycentricCoordinates(triangles[inter.triangleIndex], inter.position);
-				vec3 texture_color = pixelFromTexture(vec2(inter.u, inter.v), texture);
+
+				const vec2 uv0 = triangles[inter.triangleIndex].uv0;
+				const vec2 uv1 = triangles[inter.triangleIndex].uv1;
+				const vec2 uv2 = triangles[inter.triangleIndex].uv2;
+				const float m_U = inter.u;
+				const float m_V = inter.v;
+				float uf = uv0.x + m_U*(uv1.x - uv0.x) * m_V*(uv2.x - uv0.x); 
+				float vf = uv0.y + m_U*(uv1.y - uv0.y) * m_V*(uv2.y - uv0.y);
+				vec2 bary_coords = barycentricCoordinates(triangles[inter.triangleIndex], inter.position); //vec2(inter.u, inter.v)
+				vec3 texture_color = pixelFromTexture(bary_coords, texture);
 			#else
 				vec3 texture_color = triangles[inter.triangleIndex].color;
 			#endif
@@ -480,7 +488,7 @@ void ConvertCImg(CImg<unsigned char>& image, Texture& texture) {
 			float r = (float) image(j, i, 0, 0)/255.f;
 			float g = (float) image(j, i, 0, 1)/255.f;
 			float b = (float) image(j, i, 0, 2)/255.f;
-			texture.pixels[(texture.height-i-1) * texture.width + j] = vec3(r, g, b);
+			texture.pixels[i * texture.width + j] = vec3(r, g, b);
 		}
 	//delete &image;
 }
